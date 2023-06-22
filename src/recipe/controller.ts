717a018ff869d141f2da12fcc08cb4cbfc3401f2
Samuel Le <samuelle107@gmail.com>
2023-06-22 02:43:47 -0500

    err handling

--- src/recipe/controller.ts
+++ src/recipe/controller.ts
@@ -1,49 +1,45 @@
 import { Request, Response } from "express";
 import { Prisma } from "@prisma/client";
 
-import prisma from "../utils/prisma-client";
+import prisma from "../utils/client";
 
 export async function getRecipes(_req: Request, res: Response) {
   try {
     const recipes = await prisma.recipe.findMany();
 
     res.send(recipes);
-
-    return;
   } catch (err) {
-    console.log(err);
+    res.send({
+      message: "Failed to retrieve all recipes",
+    });
   }
-
-  res.send({
-    message: "Failed to retrieve all recipes",
-  });
 }
 
 export async function getRecipe(req: Request, res: Response) {
   const { id }: { id?: string } = req.params;
 
-  const recipe = await prisma.recipe.findUnique({
-    where: {
-      id: Number(id),
-    },
-  });
+  try {
+    const recipe = await prisma.recipe.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
 
-  if (recipe) {
-    res.send(recipe);
-
-    return;
+    if (recipe) {
+      res.send(recipe);
+    }
+  } catch (err) {
+    res.status(404).send({
+      message: `Recipe ${id} not found`,
+    });
   }
-
-  res.status(404).send({
-    message: `Recipe ${id} not found`,
-  });
 }
 
 export async function addRecipe(req: Request, res: Response) {
-  const data: Prisma.recipeCreateInput = {
-    image_url: req.body.image_url,
+  const data: Prisma.RecipeCreateInput = {
+    imageUrl: req.body.image_url,
     name: req.body.name,
-    src_url: req.body.src_url,
+    srcUrl: req.body.src_url,
   };
 
   // TODO: Add schema validation here
@@ -55,16 +51,12 @@ export async function addRecipe(req: Request, res: Response) {
 
     if (recipe) {
       res.send(recipe);
-
-      return;
     }
   } catch (err) {
-    console.log(err);
+    res.status(400).send({
+      message: "Failed to create recipe",
+    });
   }
-
-  res.status(400).send({
-    message: "Failed to create recipe",
-  });
 }
 
 export async function deleteRecipe(req: Request, res: Response) {
@@ -80,13 +72,9 @@ export async function deleteRecipe(req: Request, res: Response) {
     res.send({
       message: `Successfully deleted recipe ${id}`,
     });
-
-    return;
   } catch (err) {
-    console.log(err);
+    res.status(400).send({
+      message: "Failed to delete recipe",
+    });
   }
-
-  res.status(400).send({
-    message: "Failed to delete recipe",
-  });
 }
