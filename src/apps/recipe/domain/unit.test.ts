50ea5f1e795e428e109222875781bedb74cf55d4
Samuel Le <samuelle107@gmail.com>
2023-06-23 10:39:59 -0500

    recipe domain test

--- src/apps/recipe/domain/unit.test.ts
+++ src/apps/recipe/domain/unit.test.ts
@@ -1,20 +1,50 @@
 import { prismaMock } from "../../../utils/client/singleton";
-import { createRecipe } from ".";
+import { createRecipe, removeRecipe } from ".";
 
-test("should create recipe", async () => {
-  const data = {
-    id: 1,
-    imageUrl: "",
-    name: "",
-    srcUrl: "",
-  };
+describe("recipe.domain", () => {
+  describe("create recipe", () => {
+    test("should create recipe", async () => {
+      const resolvedData = {
+        id: 1,
+        imageUrl: "",
+        name: "Noodles",
+        srcUrl: "",
+      };
 
-  prismaMock.recipe.create.mockResolvedValue(data);
+      prismaMock.recipe.create.mockResolvedValue(resolvedData);
 
-  await expect(createRecipe(data)).resolves.toEqual({
-    id: 1,
-    imageUrl: "",
-    name: "",
-    srcUrl: "",
+      await expect(createRecipe(resolvedData)).resolves.toEqual({
+        id: 1,
+        imageUrl: "",
+        name: "Noodles",
+        srcUrl: "",
+      });
+    });
+
+    test("should fail to create recipe w/o name", async () => {
+      expect(createRecipe({})).rejects.toThrow();
+    });
+
+    test("should fail with empty name", () => {
+      expect(createRecipe({ name: "" })).rejects.toThrow();
+    });
+  });
+
+  describe("remove recipe", () => {
+    test("it removes", () => {
+      prismaMock.recipe.delete.mockResolvedValue({
+        id: 1,
+        imageUrl: "",
+        name: "test",
+        srcUrl: "",
+      });
+
+      expect(removeRecipe("1")).resolves.toEqual(1);
+    });
+
+    // Should be testing validateID instead
+    test("it fails to remove", () => {
+      expect(removeRecipe("a")).rejects.toThrow();
+    });
   });
 });
